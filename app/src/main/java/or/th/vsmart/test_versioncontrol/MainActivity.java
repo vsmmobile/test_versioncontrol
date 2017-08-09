0882c431289cd1414ad3d34e6776c338967dd81d
vsmmobile <30854744+vsmmobile@users.noreply.github.com>
2017-08-09 15:14:57 +0700

    Initial commit

---
+++ app/src/main/java/or/th/vsmart/test_versioncontrol/MainActivity.java
@@ -0,0 +1,13 @@
+package or.th.vsmart.test_versioncontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
