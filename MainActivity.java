72a6de8d6d0090f3fd803fec2d3d57abecd0912f
vsmmobile <30854744+vsmmobile@users.noreply.github.com>
2017-08-29 21:16:19 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package or.th.vsmart.test_versioncontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"xxxyyy",Toast.LENGTH_LONG).show();
+    }
+}
